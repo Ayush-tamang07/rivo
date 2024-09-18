7633c3c29580309b7ae1b63185f5526124ea9fd2
Ayush Tamang <tamangayush052@gmail.com>
2024-09-18 23:13:02 +0545

    added BiSolidQuoteAltLeft icon

--- src/components/ui/card/FeedBackCard.jsx
+++ src/components/ui/card/FeedBackCard.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { BiSolidQuoteAltLeft } from "react-icons/bi";
+
 
 function FeedBackCard(props) {
   return (
     <div className="bg-white w-96 p-3 shadow-lg">
-      <h1 className="text-4xl font-times primary-text">"</h1>
-      <h1 className="roboto-slab primary-text font-bold text-2xl">
+      <BiSolidQuoteAltLeft className="text-3xl text-primary"/>
+      <h1 className="roboto-slab primary-text font-bold text-2xl my-4">
         {props.username}
       </h1>
       <p>{props.description}</p>
